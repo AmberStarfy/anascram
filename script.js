bc6bd17df2d11ce317add0a0aa4c2e560affbecb
Nate <n4t3bunc3@gmail.com>
2026-09-21 21:33:15 +0100

    Added scramble function and the start of running the main game

---
+++ script.js
@@ -0,0 +1,42 @@
+function startGame(difficulty) {
+    let anagram;
+    switch (difficulty) {
+        case "Easy":
+            anagram = randomise("Hello");
+            break;
+        case "Medium":
+            anagram = randomise("Historic");
+        case "Hard":
+            anagram = randomise("Defenistrate");
+    }
+
+}
+
+function randomise(word) {
+    word = word.toLowerCase();
+    let letters = word.split("");
+    //window.alert(letters[0]);
+    let length = word.length;
+    let usedIndexes = [];
+    //window.alert(length);
+    let anagram = "";
+
+    while (anagram.length != word.length) {
+        let index = Math.floor(Math.random() * length);
+        //window.alert(index);
+        while (usedIndexes.includes(index)) {
+            index = Math.floor(Math.random() * length);
+        }
+        let character = letters[index];
+        anagram = anagram + character;
+        //window.alert(anagram);
+        usedIndexes[usedIndexes.length] = index;
+
+    }
+
+    if (word == anagram) {
+        anagram = randomise(word);
+    }
+
+    return anagram;
+}
