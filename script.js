fc31b1e21ae13f02508b4d0df33a98b365676476
Ethan <ethan@garyjones.com>
2026-09-22 07:16:48 +0100

    Fix randomise display - move DOM write before return (unreachable code after return)

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 function startGame(difficulty) {
-    let anagram;
-    switch (difficulty) {
-        case "Easy":
-            anagram = randomise("Hello");
-            break;
-        case "Medium":
-            anagram = randomise("Historic");
-        case "Hard":
-            anagram = randomise("Defenistrate");
-    }
+	let anagram;
+	switch (difficulty) {
+		case "Easy":
+			anagram = randomise("Hello");
+			break;
+		case "Medium":
+			anagram = randomise("Historic");
+		case "Hard":
+			anagram = randomise("Defenistrate");
+	}
 
 }
 
 function randomise(word) {
-    word = word.toLowerCase();
-    let letters = word.split("");
-    //window.alert(letters[0]);
-    let length = word.length;
-    let usedIndexes = [];
-    //window.alert(length);
-    let anagram = "";
+	word = word.toLowerCase();
+	let letters = word.split("");
+	//window.alert(letters[0]);
+	let length = word.length;
+	let usedIndexes = [];
+	//window.alert(length);
+	let anagram = "";
 
-    while (anagram.length != word.length) {
-        let index = Math.floor(Math.random() * length);
-        //window.alert(index);
-        while (usedIndexes.includes(index)) {
-            index = Math.floor(Math.random() * length);
-        }
-        let character = letters[index];
-        anagram = anagram + character;
-        //window.alert(anagram);
-        usedIndexes[usedIndexes.length] = index;
+	let anagramResult = document.getElementById("anagramResult");
 
-    }
+	while (anagram.length != word.length) {
+		let index = Math.floor(Math.random() * length);
+		//window.alert(index);
+		while (usedIndexes.includes(index)) {
+			index = Math.floor(Math.random() * length);
+		}
+		let character = letters[index];
+		anagram = anagram + character;
+		//window.alert(anagram);
+		usedIndexes[usedIndexes.length] = index;
 
-    if (word == anagram) {
-        anagram = randomise(word);
-    }
+	}
 
-    return anagram;
+	if (word == anagram) {
+		anagram = randomise(word);
+	}
+
+	anagramResult.innerHTML = anagram;
+	return anagram;
 }
